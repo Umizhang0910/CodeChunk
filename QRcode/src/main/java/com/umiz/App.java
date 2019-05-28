752eea10bfcb2eff82ca1a7e46929e970607ce28
umiz <136525553@qq.com>
2019-05-28 15:01:29 +0800

    2019/5/29 commit

---
+++ QRcode/src/main/java/com/umiz/App.java
@@ -0,0 +1,13 @@
+package com.umiz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
